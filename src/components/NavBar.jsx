16136c5bb1b16347839bd8d80d4c54859ef3c964
WyahidBadr <wyahidb2015@gmail.com>
2025-03-10 12:48:35 -0400

    router

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,54 @@
-import {useState} from "react";
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoCloseCircle } from "react-icons/io5";
 
 export default function NavBar() {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/*LOGO*/}
       <div className="flex items-center gap-4 text-xl font-bold">
-        <img src="/public/swit-small.webp" className="w-10 h-10 rounded-full" alt="Swit" />
+        <img
+          src="https://ik.imagekit.io/WBswitCG/swit-small.webp"
+          className="w-10 h-10 rounded-full"
+          alt="Swit"
+        />
         <span>Swit</span>
       </div>
       {/*Mobile Menu*/}
       <div className="visible md:hidden">
         <div className="cursor-pointer text-2xl" onClick={() => setOpen(!open)}>
-            {open ? <IoCloseCircle /> : <GiHamburgerMenu />}
+          {open ? <IoCloseCircle /> : <GiHamburgerMenu />}
+        </div>
+        {/* Mobile link list */}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 gap-8 font-medium text-lg transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Populat</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className="py-2 px-4 rounded-3xl bg-purple-400 text-white">
+              Log In ✐
+            </button>
+          </a>
         </div>
       </div>
       {/*Desktop Menu*/}
-      <div className="hidden md:flex">D</div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Populat</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="py-2 px-4 rounded-3xl bg-purple-400 text-white">
+            Log In ✐
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,58 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Homepage from "./routes/Homepage.jsx";
+import PostListPage from "./routes/PostListPage.jsx";
+import Write from "./routes/WritePage.jsx";
+import LoginPage from "./routes/LoginPage.jsx";
+import Register from "./routes/RegisterPage.jsx";
+import SinglePostPage from "./routes/SinglePostPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:(
+      <Homepage />
+    )
+  },
+  {
+    path: "/posts",
+    element:(
+      <PostListPage />
+    )
+  },
+  {
+    path: "/:slug",
+    element:(
+      <SinglePostPage/>
+    )
+  },{
+    path: "/write",
+    element:(
+      <Write/>
+    )
+  },
+  {
+    path: "/login",
+    element:(
+      <LoginPage/>
+    )
+  },
+  {
+    path: "/register",
+    element:(
+      <Register/>
+    )
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
