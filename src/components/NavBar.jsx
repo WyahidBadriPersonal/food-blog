5ce396f1c39c07270b50d0431923c7bf4ba5ebf7
WyahidBadr <wyahidb2015@gmail.com>
2025-03-10 15:11:08 -0400

    home page - buttons - image

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoCloseCircle } from "react-icons/io5";
+import { Link } from "react-router";
+import Image from "./Image";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 export default function NavBar() {
   const [open, setOpen] = useState(false);
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/*LOGO*/}
-      <div className="flex items-center gap-4 text-xl font-bold">
-        <img
-          src="https://ik.imagekit.io/WBswitCG/swit-small.webp"
-          className="w-10 h-10 rounded-full"
-          alt="Swit"
+      <Link to="/" className="flex items-center gap-4 text-xl font-bold">
+        <Image
+          src="/swit-small.webp"
+          alt="Swit Logo"
+          w={32}
+          h={32}
+          className={"rounded-full"}
         />
         <span>Swit</span>
-      </div>
+      </Link>
       {/*Mobile Menu*/}
       <div className="visible md:hidden">
         <div className="cursor-pointer text-2xl" onClick={() => setOpen(!open)}>
@@ -26,28 +36,33 @@ export default function NavBar() {
             open ? "-right-0" : "-right-[100%]"
           }`}
         >
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Populat</a>
-          <a href="/">About</a>
-          <a href="/">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
             <button className="py-2 px-4 rounded-3xl bg-purple-400 text-white">
               Log In ✐
             </button>
-          </a>
+          </Link>
         </div>
       </div>
       {/*Desktop Menu*/}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Populat</a>
-        <a href="/">About</a>
-        <a href="/">
-          <button className="py-2 px-4 rounded-3xl bg-purple-400 text-white">
-            Log In ✐
-          </button>
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <SignedOut>
+          <Link to="/login">
+            <button className="py-2 px-4 rounded-3xl bg-purple-400 text-white">
+              Log In ✐
+            </button>
+          </Link>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   );
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
+import Image from "../components/Image";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className="mt-4 flex flex-col gap-4">
+      {/* <Breadcrumb /> */}
+      <div className="flex gap-4">
+        <Link to="/">Home</Link>
+        <span>·</span>
+        <span className="text-blue-700">Reviews & Blogs</span>
+      </div>
+      {/* <Introduction /> */}
+      <div className="flex items-center justify-between">
+        {/* titles  */}
+        <div className="">
+          <h1 className="text-gray-800 text-2xl md:text-5xl lg:text-6xl font-bold">
+            Swit reviews
+          </h1>
+          <p className="mt-8 text-md md:text-xl">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam,
+            temporibus.
+          </p>
+        </div>
+        {/* animated button  */}
+        <Link to="write" className="relative hidden md:block">
+          <svg
+            viewBox="0 0 200 200"
+            width="200"
+            height="200"
+            className="text-lg tracking-widest"
+            // className="text-lg tracking-widest animate-spin animatedButton"
+          >
+            <path
+              id="circlePath"
+              fill="none"
+              d="M 100, 100 m -75, 0 a 75,75 0 1,1 150,0 a 75,75 0 1,1 -150,0"
+            />
+            <text>
+              <textPath href="#circlePath" startOffset="0%">
+                Discover New
+              </textPath>
+              <textPath href="#circlePath" startOffset="50%">
+                Learn More
+              </textPath>
+            </text>
+          </svg>
+          <button className="rounded-full flex items-center justify-center absolute top-0 left-0 right-0 bottom-0">
+            <Image
+              src="/swit-small.webp"
+              alt="Swit Logo"
+              w={125}
+              h={125}
+              className={"rounded-full"}
+            />
+          </button>
+        </Link>
+      </div>
+      {/* <Featured /> */}
+      {/* <PostList /> */}
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
