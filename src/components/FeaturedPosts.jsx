69ecd259772d4c83cc7c748a0f9ca90ea048a305
WyahidBadr <wyahidb2015@gmail.com>
2025-03-26 13:24:06 -0400

    ready to deploy v1

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -1,113 +1,137 @@
 import React from "react";
 import Image from "./Image";
 import { Link } from "react-router";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { format } from "timeago.js";
+
+const fetchPost = async () => {
+  const res = await axios.get(
+    `${import.meta.env.VITE_API_URL}/posts?featured=true&limit=4&sort=newest`
+  );
+  return res.data;
+};
 
 const FeaturedPosts = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["featuredPosts"],
+    queryFn: () => fetchPost(),
+  });
+  
+  if (isPending) return "Loading...";
+  if (error) return "An error has occurred: " + error.message;
+  
+  console.log("data ", data);
+  
+  const posts = data.posts;
+  if (!posts || posts.length === 0 || !data) return "No featured posts found";
+
+  
   return (
     <div className="mt-8 flex flex-col gap-8 lg:flex-row">
       {/* first post  */}
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* image */}
-        <Image
-          src="BARK_BBQ.jpeg"
+       {posts[0].img && <Image
+          src={posts[0].img}
           className="rounded-3xl object-cover"
           alt="BBQ"
           w="400"
-        />
+        />}
         {/* details */}
         <div className="flex items-center gap-4">
           <h1 className="font-semibold lg:text-lg">01.</h1>
-          <Link className="text-blue-700 lg:text-lg">Dinner</Link>
-          <span className="text-gray-500">1 week ago</span>
+          <Link className="text-blue-700 lg:text-lg">{posts[0].category}</Link>
+          <span className="text-gray-500">{format(posts[0].createdAt)}</span>
         </div>
         {/* title */}
         <Link
-          to="/test"
+          to={posts.slug}
           className="text-xl lg:text-3xl font-semibold lg:font-bold"
         >
-          Bark BBQ Brooklyn, NYC
+          {posts[0].title}
         </Link>
       </div>
       {/* other posts  */}
       <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* second  */}
-        <div className="lg:h-1/3 flex justify-between gap-4">
+       {posts[1] && <div className="lg:h-1/3 flex justify-between gap-4">
           <div className="aspect-square w-1/3">
-            <Image
-              src="OCEANPRIME_1.jpeg"
+           {posts[1].img && <Image
+              src={posts[1].img}
               className="rounded-3xl object-cover w-full h-full"
               w="298"
               alt="Steakhouse"
-            />
+            />}
           </div>
           <div className="w-2/3">
             {/* details  */}
             <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
               <h1 className="font-semibold">02.</h1>
-              <Link className="text-blue-700">Dinner</Link>
-              <span className="text-gray-500 text-sm">1 day ago</span>
+              <Link className="text-blue-700">{posts[1].category}</Link>
+              <span className="text-gray-500 text-sm">{format(posts[1].createdAt)}</span>
             </div>
             {/* title  */}
             <Link
-              to="/test"
+              to={posts[1].slug}
               className="text-sm sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
             >
-              Ocean Prime Manhattan, NYC
+              {posts[1].title}
             </Link>
           </div>
-        </div>
+        </div>}
         {/* third  */}
-        <div className="lg:h-1/3 flex justify-between gap-4">
+       {posts[2] && <div className="lg:h-1/3 flex justify-between gap-4">
           <div className="aspect-square w-1/3 ">
-            <Image
-              src="OCEANPRIME_2.jpeg"
+           {posts[2].img && <Image
+              src={posts[2].img}
               className="rounded-3xl object-cover w-full h-full"
               alt="Steakhouse"
               w="298"
-            />
+            />}
           </div>
           <div className="w-2/3">
             {/* details  */}
             <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
               <h1 className="font-semibold">03.</h1>
-              <Link className="text-blue-700">Dinner</Link>
-              <span className="text-gray-500 text-sm">5 day ago</span>
+              <Link className="text-blue-700">{posts[2].category}</Link>
+              <span className="text-gray-500 text-sm">{format(posts[2].createdAt)}</span>
             </div>
             {/* title  */}
             <Link
-              to="/test"
+              to={posts[2].slug}
               className="text-sm sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
             >
-              Ocean Prime Manhattan, NYC
+              {posts[2].title}
             </Link>
           </div>
-        </div>
+        </div>}
         {/* fourth  */}
-        <div className="lg:h-1/3 flex justify-between gap-4">
+       {posts[3] && <div className="lg:h-1/3 flex justify-between gap-4">
           <div className="aspect-square w-1/3">
-            <Image
-              src="GRACE_STREET.jpeg"
+           {posts[3].img && <Image
+              src={posts[3].img}
               className="rounded-3xl object-cover w-full h-full"
               alt="Dessert"
               w="298"
-            />
+            />}
           </div>
           <div className="w-2/3">
             {/* details  */}
             <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
               <h1 className="font-semibold">04.</h1>
-              <Link className="text-blue-700">Lunch</Link>
-              <span className="text-gray-500 text-sm">1 month ago</span>
+              <Link className="text-blue-700">{posts[3].category}</Link>
+              <span className="text-gray-500 text-sm">{format(posts[3].createdAt)}</span>
             </div>
             {/* title  */}
             <Link
-              to="/test"
+              to={posts[3].slug}
               className="text-sm sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
             >
-              Grace Street Manhattan, NYC
+              {posts[3].title}
             </Link>
           </div>
-        </div>
+        </div>}
       </div>
     </div>
   );
--- src/components/PostListSideMenu.jsx
+++ src/components/PostListSideMenu.jsx
@@ -1,7 +1,23 @@
 import Search from "./Search";
-import { Link } from "react-router";
+import { Link, useSearchParams } from "react-router";
 
 const PostListSideMenu = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const handleFilterChange = (e) => {
+    if (searchParams.get("sort") === e.target.value) return;
+    setSearchParams({
+      ...Object.fromEntries(searchParams.entries()),
+      sort: e.target.value,
+    });
+  };
+
+  const handleCategoryChange = (category) => {
+    if (searchParams.get("cat") === category) return;
+    setSearchParams({
+      ...Object.fromEntries(searchParams.entries()),
+      cat: category,
+    });
+  };
   return (
     <div className="px-4 h-max sticky top-8">
       <h1 className="mb-4 text-sm font-medium">Search</h1>
@@ -12,6 +28,7 @@ const PostListSideMenu = () => {
           <input
             type="radio"
             name="sort"
+            onChange={handleFilterChange}
             value="newest"
             className="cursor-pointer w-4 h-4 checked:bg-blue-500"
           />
@@ -21,6 +38,7 @@ const PostListSideMenu = () => {
           <input
             type="radio"
             name="sort"
+            onChange={handleFilterChange}
             value="popular"
             className="cursor-pointer w-4 h-4 checked:bg-blue-500"
           />
@@ -30,6 +48,7 @@ const PostListSideMenu = () => {
           <input
             type="radio"
             name="sort"
+            onChange={handleFilterChange}
             value="trending"
             className="cursor-pointer w-4 h-4 checked:bg-blue-500"
           />
@@ -39,6 +58,7 @@ const PostListSideMenu = () => {
           <input
             type="radio"
             name="sort"
+            onChange={handleFilterChange}
             value="oldest"
             className="cursor-pointer w-4 h-4 checked:bg-blue-500"
           />
@@ -47,24 +67,41 @@ const PostListSideMenu = () => {
       </div>
       <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
       <div className="flex flex-col gap-4 text-sm">
-        <Link className="underline hover:text-blue-500" to="/posts">
+        <span
+          className="underline hover:text-blue-500 cursor-pointer"
+          to="/posts"
+          onClick={() => handleCategoryChange("all")}
+        >
           All
-        </Link>
-        <Link
-          className="underline hover:text-blue-500"
+        </span>
+        <span
+          className="underline hover:text-blue-500 cursor-pointer"
           to="/posts?cat=breakfast"
+          onClick={() => handleCategoryChange("breakfast")}
         >
           Breakfast
-        </Link>
-        <Link className="underline hover:text-blue-500" to="/posts?cat=brunch">
+        </span>
+        <span
+          className="underline hover:text-blue-500 cursor-pointer"
+          to="/posts?cat=brunch"
+          onClick={() => handleCategoryChange("brunch")}
+        >
           Brunch
-        </Link>
-        <Link className="underline hover:text-blue-500" to="/posts?cat=lunch">
+        </span>
+        <span
+          className="underline hover:text-blue-500 cursor-pointer"
+          to="/posts?cat=lunch"
+          onClick={() => handleCategoryChange("lunch")}
+        >
           Lunch
-        </Link>
-        <Link className="underline hover:text-blue-500" to="/posts?cat=dinner">
+        </span>
+        <span
+          className="underline hover:text-blue-500 cursor-pointer"
+          to="/posts?cat=dinner"
+          onClick={() => handleCategoryChange("dinner")}
+        >
           Dinner
-        </Link>
+        </span>
       </div>
     </div>
   );
